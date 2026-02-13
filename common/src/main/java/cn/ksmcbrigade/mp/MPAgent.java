a25cac62f1803c4eec1d84afad9810a16c90a174
yiyuyan <179544143@qq.com>
2026-02-13 12:19:35 +0800

    ADD EXPORT FEATURE

--- common/src/main/java/cn/ksmcbrigade/mp/MPAgent.java
+++ common/src/main/java/cn/ksmcbrigade/mp/MPAgent.java
@@ -1,11 +1,10 @@
 package cn.ksmcbrigade.mp;
 
-import cn.ksmcbrigade.mp.platform.Services;
 import cn.ksmcbrigade.mp.transformers.MPTransformer;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
 import org.slf4j.Logger;
@@ -19,25 +18,31 @@ import java.io.InputStream;
 import java.lang.instrument.Instrumentation;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MPAgent {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MPAgent.class.getSimpleName());
 
     public static Map<String,byte[]> TRANSFORMATIONS = new HashMap<>();
+    public static Set<String> FOR_EXPORT = new HashSet<>();
 
     public static Instrumentation INST;
 
-    public static void premain(String arg, Instrumentation instrumentation){
+    public static File exportDir = new File("mp-exports");
+
+    public static void premain(String arg, Instrumentation instrumentation) {
         INST = instrumentation;
         LOGGER.info("MP Agent Loading...");
         instrumentation.addTransformer(new MPTransformer(),true);
         File configsDir = new File("mp-classes");
-        while(!configsDir.exists()) configsDir.mkdirs();
+        while(!configsDir.exists() || !exportDir.exists()){
+            configsDir.mkdirs();
+            exportDir.mkdirs();
+        }
+
         initConfig(configsDir);
+        initExportConfig(exportDir.toPath().resolve("mp-export-config.json").toFile());
 
         for (String s : TRANSFORMATIONS.keySet()) {
             String name = s.replace("/",".");
@@ -69,12 +74,16 @@ public class MPAgent {
                         for (JsonElement jsonElement : JsonParser.parseString(FileUtils.readFileToString(file)).getAsJsonArray()) {
                             try {
                                 byte[] bytes = getFromURL(jsonElement.getAsString());
+                                if(bytes==null){
+                                    LOGGER.warn("Ignoring {} because the bytes is null.",file);
+                                    return;
+                                }
                                 ClassNode node = new ClassNode();
                                 ClassReader reader = new ClassReader(bytes);
                                 reader.accept(node, ASM.API_VERSION);
                                 TRANSFORMATIONS.put(node.name,bytes);
                                 LOGGER.info("Loaded {} from {}",node.name,jsonElement);
-                            } catch (IOException e) {
+                            } catch (Throwable e) {
                                 LOGGER.error("Failed to load {} from {}",jsonElement,file,e);
                             }
                         }
@@ -94,23 +103,42 @@ public class MPAgent {
         }
     }
 
-    public static void agentmain(String arg,Instrumentation instrumentation){
-        premain(arg,instrumentation);
+    public static void initExportConfig(File configFile) {
+        try {
+            if(!configFile.exists()){
+                FileUtils.writeStringToFile(configFile,new JsonArray().toString());
+            }
+            JsonArray array = JsonParser.parseString(FileUtils.readFileToString(configFile)).getAsJsonArray();
+            for (JsonElement jsonElement : array) {
+                FOR_EXPORT.add(jsonElement.getAsString().replace(".","/"));
+            }
+        } catch (Throwable e) {
+            LOGGER.error("Failed to init export config: {}", configFile, e);
+        }
     }
 
-    public static byte[] getFromURL(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        URLConnection urlConnection = url.openConnection();
-        InputStream inputStream = urlConnection.getInputStream();
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static byte[] getFromURL(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-        int read = 0;
+            int read = 0;
 
-        byte[] buffer = new byte[1024];
-        while((read = inputStream.read(buffer,0,1024))!=-1){
-            outputStream.write(buffer,0,read);
+            byte[] buffer = new byte[1024];
+            while((read = inputStream.read(buffer,0,1024))!=-1){
+                outputStream.write(buffer,0,read);
+            }
+
+            return outputStream.toByteArray();
+        } catch (Throwable e) {
+            LOGGER.info("Failed to read {}",urlString,e);
+            return null;
         }
+    }
 
-        return outputStream.toByteArray();
+    public static void agentmain(String arg,Instrumentation instrumentation){
+        premain(arg,instrumentation);
     }
 }
--- common/src/main/java/cn/ksmcbrigade/mp/transformers/MPTransformer.java
+++ common/src/main/java/cn/ksmcbrigade/mp/transformers/MPTransformer.java
@@ -1,16 +1,25 @@
 package cn.ksmcbrigade.mp.transformers;
 
 import cn.ksmcbrigade.mp.MPAgent;
+import org.apache.commons.io.FileUtils;
 
+import java.io.IOException;
 import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class MPTransformer implements ClassFileTransformer {
     @Override
-    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classFileBuffer) {
         if(className==null) return null;
+        if(classFileBuffer!=null && MPAgent.FOR_EXPORT.contains(className)){
+            MPAgent.LOGGER.info("Exporting {} before transforming.",className);
+            try {
+                FileUtils.writeByteArrayToFile(MPAgent.exportDir.toPath().resolve(className+".class").toFile(),classFileBuffer);
+            } catch (IOException e) {
+                MPAgent.LOGGER.error("Failed to export {}",className,e);
+            }
+        }
         for (String s : MPAgent.TRANSFORMATIONS.keySet()) {
             if(s.equals(className)){
                 MPAgent.LOGGER.info("Transforming {}", className);
