81a28ac5c535d846623e9b52e1e0056feef51bee
yiyuyan <179544143@qq.com>
2026-02-13 15:03:04 +0800

    add export debug feat

--- common/src/main/java/cn/ksmcbrigade/mp/transformers/MPTransformer.java
+++ common/src/main/java/cn/ksmcbrigade/mp/transformers/MPTransformer.java
@@ -1,8 +1,17 @@
 package cn.ksmcbrigade.mp.transformers;
 
 import cn.ksmcbrigade.mp.MPAgent;
+import com.sun.jna.platform.KeyboardUtils;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.User32Util;
+import io.netty.channel.epoll.Native;
 import org.apache.commons.io.FileUtils;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.IOException;
 import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.ClassFileTransformer;
@@ -12,8 +21,17 @@ public class MPTransformer implements ClassFileTransformer {
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classFileBuffer) {
         if(className==null) return null;
-        if(classFileBuffer!=null && MPAgent.FOR_EXPORT.contains(className)){
+        if(classFileBuffer!=null
+                && (MPAgent.FOR_EXPORT.contains(className))){
             MPAgent.LOGGER.info("Exporting {} before transforming.",className);
+            try {
+                FileUtils.writeByteArrayToFile(MPAgent.exportDir.toPath().resolve(className+".class").toFile(),classFileBuffer);
+            } catch (IOException e) {
+                MPAgent.LOGGER.error("Failed to export {}",className,e);
+            }
+        }
+        if(classFileBuffer!=null && (KeyboardUtils.isPressed(KeyEvent.VK_F12) || isRecafAvailable())){
+            MPAgent.LOGGER.info("[MPDebug] Exporting {} before transforming.",className);
             try {
                 FileUtils.writeByteArrayToFile(MPAgent.exportDir.toPath().resolve(className+".class").toFile(),classFileBuffer);
             } catch (IOException e) {
@@ -35,5 +53,9 @@ public class MPTransformer implements ClassFileTransformer {
             }
         }
         return null;
+    }
+
+    private boolean isRecafAvailable() {
+        return User32.INSTANCE.FindWindow(null,"Recaf")!=null;
     }
 }
